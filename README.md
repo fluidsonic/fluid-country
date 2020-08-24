bb9a0d6a811894aaa17a939a7653f9346cff3d7d
Marc Knaup <marc@knaup.io>
2020-08-24 02:32:49 +0100

    🎉 0.9.1

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Installation
 `build.gradle.kts`:
 ```kotlin
 dependencies {
-    implementation("io.fluidsonic.country:fluid-country:0.9.0")
+    implementation("io.fluidsonic.country:fluid-country:0.9.1")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.7"
 }
 
-fluidLibrary(name = "country", version = "0.9.0")
+fluidLibrary(name = "country", version = "0.9.1")
 
 fluidLibraryModule(description = "Kotlin multiplatform country library") {
 	targets {
