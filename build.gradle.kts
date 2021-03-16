b46fbf35609eb9efaf10515cf907ab5941a4d9e6
Marc Knaup <marc@knaup.io>
2021-03-16 18:10:26 +0100

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.fluidsonic.gradle.*
 import org.jetbrains.kotlin.gradle.plugin.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.21"
+	id("io.fluidsonic.gradle") version "1.1.22"
 }
 
 fluidLibrary(name = "country", version = "0.9.4")
@@ -11,7 +11,7 @@ fluidLibraryModule(description = "Kotlin multiplatform country library") {
 	targets {
 		common {
 			dependencies {
-				implementation(kotlinx("serialization-core", "1.1.0-RC"))
+				implementation(kotlinx("serialization-core", "1.1.0"))
 			}
 		}
 
